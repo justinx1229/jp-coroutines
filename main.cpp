c8fdcb67dfd687943694fbe15576eddaa2ce7555
omeganotation <patcdeng@gmail.com>
2026-04-18 14:21:55 -0500

    WINDOW

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "consts.hpp"
+
+int main() {
+    std::cout << "Hello World!\n";
+
+    // Code used from: https://wiki.libsdl.org/SDL3/SDL_CreateWindow 
+
+    // create the window
+    SDL_Window *window; 
+    bool done = false; // done is true when the user exits the window.
+
+    // inits SDL Video Subsystem
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL failed to initialize: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    // window
+    window = SDL_CreateWindow(
+        "GAMEBOY COLOR EMULATOR",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED, 
+        WIDTH, 
+        HEIGHT,
+        0
+    );
+
+    // check if window exists
+    if (!window) {
+        std::cerr << "Window not there: " << SDL_GetError() << "\n";
+        return 1;
+    }
+    
+    SDL_Delay(5000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
