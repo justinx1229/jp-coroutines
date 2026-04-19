e508292b4bbadf342d16bb71de0140f6145d80e8
omeganotation <patcdeng@gmail.com>
2026-04-19 17:17:49 -0500

    modes

--- ppu.cpp
+++ ppu.cpp
@@ -20,16 +20,8 @@ void f_lyc() {
     }   
 }
 
-void run_ppu(uint32_t m_cycles) {
-    uint32_t t_cycles = 4 * m_cycles;
-    dots += t_cycles;
-    
-    if (dots >= 456) {
-        dots -= 456;
-        ly++;
-        f_lyc(); 
-        write_byte(0xFF41, read_byte(0xFF41) | ((ly == lyc) << 2));
-        
+void compute_mode() {
+    if (ly < 144) {
         if (dots <= 80) {
             mode = Mode::OAM;
         }
@@ -39,16 +31,55 @@ void run_ppu(uint32_t m_cycles) {
         else {
             mode = Mode::HBLANK;
         }
+    }
 
-        if (ly == 144) {
-            mode = Mode::VBLANK;
-            // vblank interrupt
-            write_byte(0xFF0F, read_byte(0xFF0F) | 1);
+    if (ly == 144) {
+        mode = Mode::VBLANK;
+        // vblank interrupt
+        write_byte(0xFF0F, read_byte(0xFF0F) | 1);
+    }
+    else if (ly > 153) {
+        mode = Mode::OAM;
+        ly = 0;
+        f_lyc();
+    }
+}
+
+void run_ppu(uint32_t m_cycles) {
+    uint32_t t_cycles = 4 * m_cycles;
+    // add dots
+    dots += t_cycles;
+    
+    // if dots is too big, start over. 
+    if (dots >= 456) {
+        dots -= 456;
+        ly++;
+        f_lyc(); 
+    }
+
+    compute_mode();
+
+    uint8_t value;
+    switch (mode) {
+        case Mode::OAM: {
+            value = 2;
+            break;
         }
-        else if (ly > 153) {
-            mode = Mode::OAM;
-            ly = 0;
-            f_lyc();
+        case Mode::DRAW: {
+            value = 3;
+            break;
+        }
+        case Mode::HBLANK: {
+            value = 0;
+            break;
+        }
+        case Mode::VBLANK: {
+            value = 1;
+            break;
         }
     }
+    
+    // update STAT
+    write_byte(0xFF41, (read_byte(0xFF41) & (LO_8 ^ LO_2)) | value);
+
 }
